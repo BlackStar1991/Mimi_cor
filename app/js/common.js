b4cffa86632a2f7cd0796aacf3c450484b3a86b9
wert_and-ПК\wert_and <alexandr_works@ukr.net>
2017-07-06 13:38:23 +0300

    correct sliders

--- app/js/common.js
+++ app/js/common.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
-    console.log("work");
 
+
+    // Adress and menu psevdoslider
 
     var menuSwich = $(".bl_eye-checkbox"),
         blockAdress = $(".bl_nav__adress"),
         blockNavigation = $(".bl_menu"),
         blockAdress_animationClass = "bl_nav__adress__animate",
         blockNavigation_aniamtionClass ="bl_menu__animate";
-
 
     menuSwich.on("click", function(){
 
@@ -21,11 +21,34 @@ $(document).ready(function () {
         block.toggleClass(whatClass);
     }
 
+    // decor line under navigation words
+    $(".bl_menu__word").each(function(){
+        var span = $.map($(this).text().split(''),function(e,i){
+            return e!=' '?$('<span>',{class:'letter_decor'}).text(e).get(0).outerHTML:' ';
+        });
+        $(this).html(span.join(''));
+    });
 
+
+
+    //SLIDERS
 
     var sliderHits = $(".slider_hits"),
         sliderShares = $(".slider_shares"),
         sliderNews = $(".slider_news");
+
+    var fullItems = $(".bl_fullSliders_item"),
+        fullItemsLength = fullItems.length;
+    var activeItem = "bl_fullSliders_item__active";
+
+    fullItems.on("click", function () {
+
+        fullItems.removeClass(activeItem);
+        $(this).addClass(activeItem);
+
+    });
+
+
 
 
     function owlSlider(slider){
@@ -42,6 +65,8 @@ $(document).ready(function () {
         });
     }
     owlSlider(sliderHits);
+    owlSlider(sliderShares);
+    owlSlider(sliderNews);
 
 
 
